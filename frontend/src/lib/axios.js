4656977aca2858892f259a3c9ebfb6ded6df9d89
tejesh <tejeshvit007@gmail.com>
2025-07-04 23:44:58 -0700

    fix: simplify axios and socket baseURL configuration for consistent API access

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_MODE === "development"
-      ? `${process.env.NEXT_PUBLIC_API_URL}/api`
-      : "/api",
+  baseURL:`${process.env.NEXT_PUBLIC_API_URL}/api`,
   withCredentials: true,
 });
--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,8 +1,6 @@
 // lib/socket.js
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.NEXT_PUBLIC_MODE === "development"
-      ? `${process.env.NEXT_PUBLIC_API_URL}`
-      : "/", {
+export const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`, {
   withCredentials: true,
 });
