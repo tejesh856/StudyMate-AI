b00b09eaa9f7da98e63afde5af61e9b300c0f75a
tejesh <tejeshvit007@gmail.com>
2025-07-04 23:13:55 -0700

    fix: update package.json and postcss.config.mjs for tailwindcss integration

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,7 +1,5 @@
 // postcss.config.js (ESM version)
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const config= {
+  plugins: ['@tailwindcss/postcss']
 };
+export default config;
